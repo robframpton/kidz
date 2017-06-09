195a2c01712481b51d1f9bd53a125e88654eefc8
Travis Cory <travis.cory@liferay.com>
2017-06-09 13:45:36 -0700

    Update look

--- node/src/components/ListOfKidz.js
+++ node/src/components/ListOfKidz.js
@@ -4,46 +4,42 @@ import moment from 'moment';
 
 class ListOfKidz extends JSXComponent {
 	render() {
-		let headerCSSClass = `collapsible-header ${this.props.kids.length <= 10 ? 'active' : ''}`;
+		let {kids} = this.props;
+
+		kids = kids || [];
+
+		let headerCSSClass = `collapsible-header ${kids.length <= 10 ? 'active' : ''}`;
+
+		let kidItems = kids.map(kid => {
+			return (
+				<li class="collection-item avatar">
+					<span class="circle">
+						{kid.gender}
+					</span>
+
+					<span class="title">{kid.name}</span>
+
+					<p>
+						Birthday: {kid.birthday ? moment(kid.birthday).format('MMMM Do YYYY') : ''} <br />
+
+						Rating: {kid.rating ? kid.rating : '11'}/10
+					</p>
+
+					<a href={url} class="secondary-content">
+						<i class="material-icons">link</i>
+					</a>
+				</li>
+			);
+		});
 
 		return(
 			<ul class="collapsible" data-collapsible="accordion">
 				<li>
-					<div class={headerCSSClass}><i class="material-icons">filter_drama</i>My Kidz</div>
+					<div class={headerCSSClass}>My Kidz</div>
 
 					<div class="collapsible-body">
 						<ul class="list-of-kids collection">
-							{this.props.kids.length > 0 ?
-								this.props.kids.map(
-									(kid) => {
-										let url = `kid/${kid.id}`;
-
-										return (
-											<li class="collection-item avatar">
-												<span class="circle">
-													<p class="gener-icon">{kid.gender}</p>
-												</span>
-
-												<span class="title">{kid.name}</span>
-
-												<p>
-													Birthday: {moment(kid.birthday).format('MMMM Do YYYY')} <br />
-
-													Rating: {kid.rating ? kid.rating : '11'}/10
-												</p>
-
-												<a href={url} class="secondary-content">
-													<i class="material-icons">link</i>
-												</a>
-											</li>
-										);
-									}
-								)
-
-								:
-
-								''
-							}
+							{kidItems}
 						</ul>
 					</div>
 				</li>
@@ -60,7 +56,8 @@ class ListOfKidz extends JSXComponent {
 
 ListOfKidz.PROPS = {
 	kids: {
-		validator: core.isObject()
+		validator: core.isObject(),
+		value: []
 	}
 }
 
